3978e21e3039004b66c83655c75f60a969a52e79
Tomasz Cekało <tomasz.cekalo@gmail.com>
2023-08-16 08:57:30 +0200

    added entity tests

---
+++ Undine.LeopotamEcsLite.Tests/EntityTests.cs
@@ -0,0 +1,43 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Undine.Core;
+using Undine.LeopotamEcsLite.Tests.Components;
+
+namespace Undine.LeopotamEcsLite.Tests
+{
+    [TestClass]
+    public class EntityTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+        }
+
+        [TestMethod]
+        public void ComponentCanBeAdded()
+        {
+            var container = new LeopotamEcsLiteContainer();
+            container.Init();
+            var entity = container.CreateNewEntity();
+            entity.AddComponent(new AComponent());
+        }
+
+        [TestMethod]
+        public void ComponentCanBeRetrieved()
+        {
+            var container = new LeopotamEcsLiteContainer();
+            var mock = Substitute.For<UnifiedSystem<AComponent>>();
+            container.AddSystem(mock);
+            container.Init();
+            var entity = (LeopotamEntity)container.CreateNewEntity();
+            entity.AddComponent(new AComponent());
+            container.Run();
+            ref var component = ref entity.GetComponent<AComponent>();
+            Assert.IsNotNull(component);
+        }//
+    }
+}
